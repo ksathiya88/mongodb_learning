891fbe084aa2b1ad0b31432375e84066116cfc0c
Karthik Sathiyamurthy <k.sathiyamurthy@wkelms.com>
2019-08-14 12:49:03 +0530

    Adding files

---
+++ db_commands.js
@@ -0,0 +1,12 @@
+
+// Showing database:
+// Show dbs
+
+// Creating database:
+// use <databasename>;
+
+// For seeing collections:
+// show collections
+
+// For creating Collection:
+// db.createCollection("customers");
---
+++ insert_commands.js
@@ -0,0 +1,9 @@
+
+
+// Inserting a document into a collection:
+// db.userData.insertOne({ name: "Kar", address: "XYZ" });
+
+// For fetching the data
+// db.userData.find({});
+
+
