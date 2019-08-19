aeb620b095eab3d42a8ded8ae5b444d38977ea65
Karthik Sathiyamurthy <k.sathiyamurthy@wkelms.com>
2019-08-19 18:19:13 +0530

    Adding files

---
+++ find/Array Comparators/array_comparators.js
@@ -0,0 +1,31 @@
+// use this to import
+mongoimport users.json -d users -c user_data --jsonArray --drop
+
+// query object in Array
+db.user_data.find({"hobbies.title":"Sports"}).pretty();
+
+// $size
+db.user_data.find({hobbies:{$size:3}}).pretty();
+
+//$all
+mongoimport boxoffice.json -d boxoffice -c moviestarts --jsonArray --drop
+
+db.moviestarts.find({genre:{$all:["action","thriller"]}}).pretty();
+
+// $elemMatch
+
+use users
+// it gives a issue as it will filter even other hobbies with frequency 
+// greater than 3
+db.users.find(
+    {$and:[{"hobbies.frequency":{$gt:3}},{"hobbies.title":"Sports"}]}).pretty()
+
+
+db.users.find({hobbies:
+    {$elemMatch:{"title":"Sports",frequency:{$gte:3}}}}).pretty();
+
+
+
+
+
+
--- find/element Operators/element.js
+++ find/element Operators/element.js
@@ -17,3 +17,22 @@ db.users.find({phone:{$type:"number"}}).pretty()
 // $type as array
 db.users.find({phone:{$type:["number","string"]}}).pretty()
 
+// $regex
+db.movies.find({summary:{$regex:/Berserk/}}).pretty();
+
+// $expr
+
+use finance;
+db.sales.insertMany([{volume:100,target:120},
+    {volume:89,target:80},{volume:200,target:170}])
+
+db.sales.find({$expr:{$gt:["$volume","$target"]}})
+
+
+db.sales.find({$expr:
+    {$gt:[{$cond:
+        {if:{$gte:["$volume",190]},then:{$subtract:["$volume",30]},
+        else:"$volume"}},"$target"]}})
+
+
+
